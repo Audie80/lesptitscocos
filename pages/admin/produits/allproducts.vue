3704df131ec2c25f26c7c735aa2cefa4a7329ff3
CamilleTr <camille.treussard@gmail.com>
2019-07-25 10:48:31 +0200

    suppression de produitsCategories

---
+++ pages/admin/produits/allproducts.vue
@@ -0,0 +1,19 @@
+
+<template>
+    <div>
+        <h1>Tous les produits</h1>
+        <nuxt-link to="/admin/produitsCategories">Voir les catégories</nuxt-link>
+    </div>
+</template>
+
+<script>
+
+export default {
+    layout: 'admin',
+    data: function() {
+        return {
+            
+        }
+    }
+}
+</script>
